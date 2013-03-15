234855f0a3cc8ed02e8a8dae509f7d936db7b164
Sebastian Tramp <mail@sebastian.tramp.name>
2013-03-15 14:47:15 +0100

    fix sniffer issues

--- ping_server.js
+++ ping_server.js
@@ -1,19 +1,23 @@
-var Pingback = require('./lib/pingback')
-  , express = require('express');
+/*global require, module, console */
+
+var Pingback = require('./lib/pingback'),
+    express = require('express');
 
 var target = express();
 
 target.use(target.router);
 
-var ping = Pingback.middleware(function(source, target) {
-  console.log('Successful pingback from: ' + source.href);
-  //console.log('Page title:', this.title);
-  //console.log('Excerpt: ' + this.excerpt);
+var ping = Pingback.middleware(function (source, target) {
+    'use strict';
+    console.log('Successful pingback from: ' + source.href);
+    //console.log('Page title:', this.title);
+    //console.log('Excerpt: ' + this.excerpt);
 });
 
 target.use('/ping', ping);
-target.use(function(err, req, res, next) {
-  console.log(err.stack || err + '');
+target.use(function (err, req, res, next) {
+    'use strict';
+    console.log(err.stack || err);
 });
 
 target.listen(5000);
